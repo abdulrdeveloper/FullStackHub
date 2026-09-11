545ade1d4bd53c3c5f7d888033cc705ab4bfd0ec
Abdul Rahman <abdulrahman2040b@gmail.com>
2026-09-11 21:53:41 +0500

    feat: implement a simple jest (testing framework ) project to learn the working of jest

---
+++ backend best collection/jest-testing-framework/test/_app.test.js
@@ -0,0 +1,9 @@
+import request from 'supertest';
+import app from '../src/app.js';
+
+test('GET / returns Hello, World!', async () => {
+  const response = await request(app).get('/');
+
+  expect(response.status).toBe(200);
+  expect(response.text).toBe('Hello, World!');
+});
